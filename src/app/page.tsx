e71f8adb9c36206722464630712ae6ebec2d803c
O2_xander <murugamwaura1308@gmail.com>
2026-02-21 16:16:48 +0000

    fix: remove unused social media icons from the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaTiktok } from 'react-icons/fa'
+import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa'
 
 // Register GSAP plugins
 if (typeof window !== 'undefined') {
@@ -107,8 +107,6 @@ export default function Home() {
   const socialLinks = [
     { name: 'Facebook', iconName: 'facebook', url: 'https://www.facebook.com/profile.php?id=61555800447073' },
     { name: 'Instagram', iconName: 'instagram', url: 'https://www.instagram.com/sunnysidedairy_ke?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' },
-    { name: 'Twitter', iconName: 'twitter', url: '#' },
-    { name: 'LinkedIn', iconName: 'linkedin', url: '#' },
     { name: 'Tiktok', iconName: 'tiktok', url: 'https://www.tiktok.com/@sunnysidedairy?is_from_webapp=1&sender_device=pc' },
   ]
 
@@ -116,8 +114,6 @@ export default function Home() {
     switch (iconName) {
       case 'facebook': return <FaFacebookF />
       case 'instagram': return <FaInstagram />
-      case 'twitter': return <FaTwitter />
-      case 'linkedin': return <FaLinkedinIn />
       case 'tiktok': return <FaTiktok />
     }
   }
